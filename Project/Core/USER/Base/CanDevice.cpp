cbdc70e9b54bc61024c5f649933616ab2e962c83
Turing <ay52159wyh@163.com>
2024-11-11 20:12:41 +0800

    添加了注释

--- Project/Core/USER/Base/CanDevice.cpp
+++ Project/Core/USER/Base/CanDevice.cpp
@@ -1,5 +1,5 @@
 #include "CanDevice.h"
-
+//静态变量的初始化
 uint8_t CanManager::RxData1[8]={0};
 uint8_t CanManager::RxData2[8]={0};
 
@@ -9,8 +9,8 @@ CanDevice* CanDevice::Can2_Instances1[MAX_INSTANCES+1] = {nullptr};
 CanDevice* CanDevice::Can2_Instances2[MAX_INSTANCES+1] = {nullptr};
 
 CanDevice::CanDevice(CAN_HandleTypeDef *hcan_, uint8_t can_id_):hcan(hcan_), can_id(can_id_){
-    if(hcan_ == &hcan1){
-        if(can_id_ <= 4){
+    if(hcan_ == &hcan1){          //根据挂载在CAN1上的设备的ID号，分配实例在数组的位置，数组的下标即是CAN设备的ID号，这也是MAX_INSTANCES+1的原因
+        if(can_id_ <= 4){         //这样做是为了方便process_data()里配置下发报文中8字节数组里包含的电流值控制信息
             Can1_Instances1[can_id_] = this;
         }else if(can_id_ <= 8){
             Can1_Instances2[can_id_-4] = this;
@@ -29,27 +29,27 @@ CanDevice::CanDevice(CAN_HandleTypeDef *hcan_, uint8_t can_id_):hcan(hcan_), can
 
 void CanDevice::CAN1_Filter_Init(){
     CAN_FilterTypeDef  sFilterConfig;
-    sFilterConfig.FilterActivation = ENABLE;
-    sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
-    sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
-    sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
-    sFilterConfig.FilterBank = 0;
+    sFilterConfig.FilterActivation = ENABLE;  //启用过滤器
+    sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;  //掩码模式
+    sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT; //32位过滤器
+    sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0; //通过过滤器的报文放置FIFO0
+    sFilterConfig.FilterBank = 0;  //使用0号过滤器
     sFilterConfig.FilterIdHigh = 0x00;
-    sFilterConfig.FilterIdLow = 0x00;
+    sFilterConfig.FilterIdLow = 0x00;   //不过滤任何ID号，即接收所有ID号的报文
     sFilterConfig.FilterMaskIdHigh = 0x00;
     sFilterConfig.FilterMaskIdLow = 0x00;
-    sFilterConfig.SlaveStartFilterBank = 14;
+    sFilterConfig.SlaveStartFilterBank = 14;  //can2的过滤器从14号开始,避免与can1的过滤器冲突
     if(HAL_CAN_ConfigFilter(&hcan1, &sFilterConfig)!= HAL_OK){
         Error_Handler();
     }
 
-    if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+    if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) //使能can1的FIFO0接收中断
     {
         /* Start Error */
         Error_Handler();
     }
 
-     if (HAL_CAN_Start(&hcan1) != HAL_OK)
+     if (HAL_CAN_Start(&hcan1) != HAL_OK)  //启动CAN1
     {
         /* Start Error */
         Error_Handler();
@@ -59,13 +59,13 @@ void CanDevice::CAN1_Filter_Init(){
 void CanDevice::CAN2_Filter_Init(){
 
     CAN_FilterTypeDef  sFilterConfig;
-    sFilterConfig.FilterActivation = ENABLE;
-    sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
-    sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
-    sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0;
-    sFilterConfig.FilterBank = 14;
+    sFilterConfig.FilterActivation = ENABLE;  //启用过滤器
+    sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK; //掩码模式
+    sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT; //32位过滤器
+    sFilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0; //通过过滤器的报文放置FIFO0
+    sFilterConfig.FilterBank = 14;  //使用14号过滤器
     sFilterConfig.FilterIdHigh = 0x00;
-    sFilterConfig.FilterIdLow = 0x00;
+    sFilterConfig.FilterIdLow = 0x00; //不过滤任何ID号，即接收所有ID号的报文
     sFilterConfig.FilterMaskIdHigh = 0x00;
     sFilterConfig.FilterMaskIdLow = 0x00;
     
@@ -73,13 +73,13 @@ void CanDevice::CAN2_Filter_Init(){
         Error_Handler();
     }
 
-    if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+    if (HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK) //使能can2的FIFO0接收中断
     {
         /* Start Error */
         Error_Handler();
     }
 
-    if (HAL_CAN_Start(&hcan2) != HAL_OK)
+    if (HAL_CAN_Start(&hcan2) != HAL_OK)  //启动CAN2
     {
         /* Start Error */
         Error_Handler();
@@ -87,32 +87,32 @@ void CanDevice::CAN2_Filter_Init(){
 }
 
 CanManager::CanManager(){
-    TxHeader1.StdId = 0x200;
-    TxHeader1.ExtId = 0x00;
-    TxHeader1.RTR = CAN_RTR_DATA;
-    TxHeader1.IDE = CAN_ID_STD;
-    TxHeader1.DLC = 8;
-    TxHeader1.TransmitGlobalTime = DISABLE;
+    TxHeader1.StdId = 0x200;  //用于控制id为0x201~0x204的电机的can报文数据头
+    TxHeader1.ExtId = 0x00;   //不使用扩展帧
+    TxHeader1.RTR = CAN_RTR_DATA;  //使用数据帧
+    TxHeader1.IDE = CAN_ID_STD;   //使用标准帧
+    TxHeader1.DLC = 8;  //数据长度为8字节
+    TxHeader1.TransmitGlobalTime = DISABLE;  //不使用全局时间戳
 
-    TxHeader2.StdId = 0x1FF;
-    TxHeader2.ExtId = 0x00;
-    TxHeader2.RTR = CAN_RTR_DATA;
-    TxHeader2.IDE = CAN_ID_STD;
-    TxHeader2.DLC = 8;
-    TxHeader2.TransmitGlobalTime = DISABLE;
+    TxHeader2.StdId = 0x1FF;  //用于控制id为0x205~0x208的电机的can报文数据头
+    TxHeader2.ExtId = 0x00;   //不使用扩展帧
+    TxHeader2.RTR = CAN_RTR_DATA;  //使用数据帧
+    TxHeader2.IDE = CAN_ID_STD;   //使用标准帧
+    TxHeader2.DLC = 8;  //数据长度为8字节
+    TxHeader2.TransmitGlobalTime = DISABLE;  //不使用全局时间戳
 
 }
 
 void CanManager::process_data(){
     for(uint8_t i=1; i<MAX_INSTANCES+1; i++){
         int16_t temp_vcurrent = 0;
-        if(CanDevice::Can1_Instances1[i] != nullptr){
-            temp_vcurrent = CanDevice::Can1_Instances1[i]->motor_process();
+        if(CanDevice::Can1_Instances1[i] != nullptr){    //如果id号为0x201~0x204的实例存在,can数据帧中对应id号的电流值由实例的motor_process()函数计算得到
+            temp_vcurrent = CanDevice::Can1_Instances1[i]->motor_process();       //实例不存在则对应id号的电流值为0（任何值都可以，因为用不上）
         }
-        send_buf1[2 * i - 2] = (uint8_t)(temp_vcurrent >> 8);
-        send_buf1[2 * i - 1] = (uint8_t)temp_vcurrent;
+        send_buf1[2 * i - 2] = (uint8_t)(temp_vcurrent >> 8);   //控制电流值的高8位
+        send_buf1[2 * i - 1] = (uint8_t)temp_vcurrent;      //控制电流值的低8位
     }
-    if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader1, send_buf1, &msg_box1) == HAL_ERROR)
+    if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader1, send_buf1, &msg_box1) == HAL_ERROR)  //将控制信息发给CAN1上id号为0x201~0x204的电机
     {
         Error_Handler();
         // Failed to add message to the transmit mailbox
@@ -120,18 +120,19 @@ void CanManager::process_data(){
 
     for(uint8_t i=1; i<MAX_INSTANCES+1; i++){
         int16_t temp_vcurrent = 0;
-        if(CanDevice::Can1_Instances2[i] != nullptr){
-            temp_vcurrent = CanDevice::Can1_Instances2[i]->motor_process();
+        if(CanDevice::Can1_Instances2[i] != nullptr){   //如果id号为0x205~0x208的实例存在,can数据帧中对应id号的电流值由实例的motor_process()函数计算得到
+            temp_vcurrent = CanDevice::Can1_Instances2[i]->motor_process(); 
         }
-        send_buf1[2 * i - 2] = (uint8_t)(temp_vcurrent >> 8);
-        send_buf1[2 * i - 1] = (uint8_t)temp_vcurrent;
+        send_buf1[2 * i - 2] = (uint8_t)(temp_vcurrent >> 8);   //控制电流值的高8位
+        send_buf1[2 * i - 1] = (uint8_t)temp_vcurrent;      //控制电流值的低8位
     }
-    if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader2, send_buf1, &msg_box1) == HAL_ERROR)
+    if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader2, send_buf1, &msg_box1) == HAL_ERROR)     //将控制信息发给CAN1上id号为0x205~0x208的电机
     {
         Error_Handler();
         // Failed to add message to the transmit mailbox
     }
 
+    //CAN2的处理方式与CAN1相同
     for(uint8_t i=1; i<MAX_INSTANCES+1; i++){
         int16_t temp_vcurrent = 0;
         if(CanDevice::Can2_Instances1[i] != nullptr){
@@ -165,20 +166,21 @@ extern "C" void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan){
     if(hcan == &hcan1){
 
         CAN_RxHeaderTypeDef RxHeader1;
-        HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &RxHeader1, CanManager::RxData1);
+        HAL_CAN_GetRxMessage(&hcan1, CAN_RX_FIFO0, &RxHeader1, CanManager::RxData1);  //获取电调上传的电机信息,放在RxData1数组中
 
-        for(uint8_t i=1; i<MAX_INSTANCES+1; i++){
-            if(CanDevice::Can1_Instances1[i]->can_id == RxHeader1.StdId - 0x200){
+        for(uint8_t i=1; i<MAX_INSTANCES+1; i++){   //遍历所有实例
+            if(CanDevice::Can1_Instances1[i]->can_id == RxHeader1.StdId - 0x200){    //如果收到的CAN帧的ID号与实例的ID号匹配，则调用实例的Can_update()函数更新实例的数据
                 CanDevice::Can1_Instances1[i]->Can_update(CanManager::RxData1);
             }
         }
 
-        for(uint8_t i=1; i<MAX_INSTANCES+1; i++){
-            if(CanDevice::Can1_Instances2[i]->can_id == RxHeader1.StdId - 0x200){
+        for(uint8_t i=1; i<MAX_INSTANCES+1; i++){    //遍历所有实例
+            if(CanDevice::Can1_Instances2[i]->can_id == RxHeader1.StdId - 0x200){    //如果收到的CAN帧的ID号与实例的ID号匹配，则调用实例的Can_update()函数更新实例的数据
                 CanDevice::Can1_Instances2[i]->Can_update(CanManager::RxData1);
             }
         }
 
+    //CAN2的处理方式与CAN1相同
     }else if(hcan == &hcan2){
 
         CAN_RxHeaderTypeDef RxHeader2;
--- Project/Core/USER/Base/CanDevice.h
+++ Project/Core/USER/Base/CanDevice.h
@@ -20,7 +20,7 @@ extern "C"
 #ifdef __cplusplus
 #define MAX_INSTANCES 4 // 一条can上最多挂四个3508
 
-enum Can_id
+enum Can_id  // 定义M3508和GM6020的can设备id
 {
     m3508_id_1 = 0x201,
     m3508_id_2 = 0x202,
@@ -38,19 +38,19 @@ class CanDevice
 {
 public:
     CAN_HandleTypeDef *hcan = nullptr;  // CAN 句柄
-    uint8_t can_id = 0;
-    static CanDevice *Can1_Instances1[MAX_INSTANCES+1]; // 保存can1和can2上的所有实例
-    static CanDevice *Can1_Instances2[MAX_INSTANCES+1];
-    static CanDevice *Can2_Instances1[MAX_INSTANCES+1];
-    static CanDevice *Can2_Instances2[MAX_INSTANCES+1];
+    uint8_t can_id = 0;  //保存每个实例的can_id
+    static CanDevice *Can1_Instances1[MAX_INSTANCES+1]; // 保存can1上id为1-4的实例
+    static CanDevice *Can1_Instances2[MAX_INSTANCES+1]; // 保存can1上id为5-8的实例
+    static CanDevice *Can2_Instances1[MAX_INSTANCES+1]; // 保存can2上id为1-4的实例
+    static CanDevice *Can2_Instances2[MAX_INSTANCES+1]; // 保存can2上id为5-8的实例
     uint8_t Can1_instance_index1 = 0; // 实例索引
     uint8_t Can1_instance_index2 = 0;
     uint8_t Can2_instance_index1 = 0;
     uint8_t Can2_instance_index2 = 0;
 
     virtual int16_t motor_process() = 0; // 给m3508用的接口，其他电机不要管
-    virtual void Can_update(uint8_t can_RxData[8]) = 0;
-    void CAN1_Filter_Init(void);
+    virtual void Can_update(uint8_t can_RxData[8]) = 0;  //用于更新电机上传的数据
+    void CAN1_Filter_Init(void); // 初始化CAN1过滤器
     void CAN2_Filter_Init(void);
 
     CanDevice(CAN_HandleTypeDef *hcan_, uint8_t can_id_);
@@ -60,18 +60,18 @@ class CanManager : public ITaskProcessor
 {
 private:
     
-    CAN_TxHeaderTypeDef TxHeader1,TxHeader2;
-    uint8_t send_buf1[8] = {0};
+    CAN_TxHeaderTypeDef TxHeader1,TxHeader2;  // 定义CAN下发报文的数据头
+    uint8_t send_buf1[8] = {0};  // 定义CAN下发报文用于控制电机的数据
     uint8_t send_buf2[8] = {0};
-    uint32_t msg_box1 = 0;
+    uint32_t msg_box1 = 0;  //保存下发报文使用的邮箱
     uint32_t msg_box2 = 0;
 
 public:
-    void process_data();
+    void process_data();  //在freertos中调用的函数，用于发送CAN下发报文
     CanManager();
 
-    static uint8_t RxData1[8];
-    static uint8_t RxData2[8];
+    static uint8_t RxData1[8];  //存储CAN1接收到的数据
+    static uint8_t RxData2[8];  //存储CAN2接收到的数据
 };
 
 #endif
